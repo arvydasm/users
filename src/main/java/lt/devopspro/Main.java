4dae3df378b8b527a29cc4bb4f994668709d6e18
babonis <babonis@gmail.com>
2018-03-14 14:47:13 +0200

    gwdsfgw

--- src/main/java/lt/devopspro/Main.java
+++
@@ -1,4 +0,0 @@
-package lt.devopspro;
-
-public class Main {
-}
--- src/main/java/lt/devopspro/controller/AppRestController.java
+++ src/main/java/lt/devopspro/controller/AppRestController.java
@@ -1,4 +1,24 @@
 package lt.devopspro.controller;
 
+
+import lt.devopspro.model.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class AppRestController {
+
+    public static List<User> usersList = new ArrayList<>();
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public void signup(User user) {
+        usersList.forEach();
+    }
+
+
+
 }
