29493fa3c846db2fb10c6c4293efa6dc516eeea1
Miglė Vaitulevičiūtė <migle.vaituleviciute@gmail.com>
2018-03-14 15:30:20 +0200

    asJSON

--- src/test/java/lt/devopspro/ControllerTest.java
+++ src/test/java/lt/devopspro/ControllerTest.java
@@ -37,4 +37,13 @@ public class ControllerTest {
         mvc.perform(MockMvcRequestBuilders.post("/login", loginForm).accept(MediaType.APPLICATION_JSON));
 
     }
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
